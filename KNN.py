4f0272017af5dd7f952a8864ec2bd9c4108e75f0
maresan <maresan91@gmail.com>
2017-09-14 18:56:21 -0300

    knn
    knn

---
+++ KNN.py
@@ -0,0 +1,72 @@
+from random import shuffle
+from math import sqrt
+import operator
+import csv
+
+def cargarDatos(archivo):
+    entrenamiento = []
+    pruebas = []
+    with open(archivo, 'rb') as csvfile:
+        lineas = csv.reader(csvfile)
+        dataset = list(lineas)
+        shuffle(dataset)
+        for i in range(len(dataset)):
+            for j in range(len(dataset[0])-1):
+                dataset[i][j] = float(dataset[i][j])
+            if i < len(dataset)*0.9:
+                entrenamiento.append(dataset[i])
+            else:
+                pruebas.append(dataset[i])
+    return entrenamiento, pruebas
+
+def calcularDistancia(pruebas, entrenamiento):
+    distancia = 0
+    for i in range(len(pruebas)-1):
+        distancia = (distancia + pruebas[i] - entrenamiento[i])**2
+    return sqrt(distancia)
+ 
+def obtenerDistancias(entrenamiento, pruebas):
+    distancias = []
+    for i in range(len(entrenamiento)):
+        distancia = calcularDistancia(pruebas, entrenamiento[i])
+        distancias.append((entrenamiento[i], distancia))
+    distancias.sort(key=operator.itemgetter(1))
+    return distancias
+
+def obtenerVecinos(distancias, k):
+    vecinos = []
+    for i in range(k):
+        vecinos.append(distancias[i][0])
+    return vecinos
+
+def generarPrediccion(vecinos):
+    classVotes = {}
+    for i in range(len(vecinos)):
+        prediccion = vecinos[i][-1]
+        if prediccion in classVotes:
+            classVotes[prediccion] = classVotes[prediccion] + 1
+        else:
+            classVotes[prediccion] = 1
+    sorteo = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorteo[0][0]
+ 
+def calcularPorcentaje(pruebas, predicciones):
+    correcta = 0
+    for i in range(len(pruebas)):
+        if pruebas[i][-1] == predicciones[i]:
+            correcta += 1
+    return (correcta/float(len(pruebas))) * 100.0
+
+#-----------------------------------------------------------------------------------------------------------------------
+#----------------------------------------- Menu Principal --------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------------
+
+predicciones=[]
+k = 3
+entrenamiento, pruebas  = cargarDatos("iris_dataset.csv")
+for i in range(len(pruebas)):
+    distancias = obtenerDistancias(entrenamiento, pruebas[i])
+    prediccion = generarPrediccion(obtenerVecinos(distancias, k))
+    predicciones.append(prediccion)
+    print("Prediccion: {} actual: {}"). format(str(prediccion), str(pruebas[i][-1]) )
+print "Porcentaje de certeza: {} %".format(str(calcularPorcentaje(pruebas, predicciones)))
